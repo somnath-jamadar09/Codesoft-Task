ecb4be0fe95886c42c1f57958ec0088f4635e885
somnath-jamadar09 <jamadarsomnath3@gmail.com>
2026-02-04 22:26:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let currentInput = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        if (value === "C") {
+            currentInput = "";
+            display.value = "";
+        } 
+        else if (value === "=") {
+            try {
+                currentInput = eval(currentInput);
+                display.value = currentInput;
+            } catch {
+                display.value = "Error";
+                currentInput = "";
+            }
+        } 
+        else {
+            currentInput += value;
+            display.value = currentInput;
+        }
+    });
+});
